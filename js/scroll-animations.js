ab86e171de5b0736371a45705de9620a196e1907
Antonio Gargaro <antoniogargaro97@gmail.com>
2019-08-29 16:48:06 +0100

    animations and transition effects added

--- js/scroll-animations.js
+++ js/scroll-animations.js
@@ -1,8 +1,9 @@
+
+
 function scrollProgress() {
     let root = document.getElementById("root");
     let pb = document.getElementById("progress-bar");
     let pbf = document.getElementById("progress-bar-fill");
-
 
     let winScroll = root.scrollTop;
     let height = root.scrollHeight - root.clientHeight;
@@ -18,7 +19,6 @@ function scrollProgress() {
         FROM = "scrolled";
         TO = "not-scrolled";
 
-        
         pb.classList.add("no-height");
     } else {
         FROM = "not-scrolled";
@@ -33,4 +33,57 @@ function scrollProgress() {
         elementsToUpdate[i].classList.replace(FROM, TO);
     }
 
+    updateNavButtons();
+   
+
+}
+
+const isInViewport = (elem) => {
+    var bounding = elem.getBoundingClientRect();
+    return (
+        bounding.top >= 0 &&
+        bounding.left >= 0 &&
+        bounding.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+        bounding.right <= (window.innerWidth || document.documentElement.clientWidth)
+    );
+};
+
+const updateNavButtons = () => {
+    let secAboutMe = document.getElementById("aboutme-caption");
+    let secProjects = document.getElementById("projects-caption");
+    let secAchievements = document.getElementById("achievements-caption");
+
+    
+    let navAboutMe = document.getElementById("aboutme-navbutton");
+    let navProjects = document.getElementById("projects-navbutton");
+    let navAchievements = document.getElementById("achievements-navbutton");
+
+    if (isInViewport(secAboutMe)) {
+        navAboutMe.classList.add("inViewport");
+
+        navProjects.classList.remove("inViewport");
+        navAchievements.classList.remove("inViewport");
+        return;
+    }
+
+    if (isInViewport(secProjects)) {
+        navProjects.classList.add("inViewport");
+
+        navAboutMe.classList.remove("inViewport");
+        navAchievements.classList.remove("inViewport");
+        return;
+    }
+
+    if (isInViewport(secAchievements)) {
+        navAchievements.classList.add("inViewport");
+
+        navProjects.classList.remove("inViewport");
+        navAboutMe.classList.remove("inViewport");
+        return;
+    }
+}
+
+
+const init = () => {
+    updateNavButtons();
 }
